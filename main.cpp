bcd2b909a7da4cfaa7a71b17cb41b59b9356eea9
Tino <tinomuzambi@gmail.com>
2020-04-27 16:49:57 +0200

    Implements general input.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 // MZMTIN002
 
-#include "main.h"
+using namespace std;
+#include "iostream"
+#include "string"
 
 int main(int argc, char* argv[]) {
-    cout << "It works" << endl;
+    string dataset;
+    string output;
+    int noClusters = 10;
+    int binWidth = 1;
+    if (argc == 2) {
+        dataset = argv[1];
+    }
+    else if (argc == 3) {
+        output = argv[2];
+    }
+    else if (argc == 4) {
+        output = argv[2];
+        noClusters = stoi(argv[3]);
+    }
+    else if (argc == 5) {
+        output = argv[2];
+        noClusters = stoi(argv[3]);
+        binWidth = stoi(argv[4]);
+    }
+    else {
+        cout << "Please provide the necessary requirements." << endl;
+        return 1;
+    }
 
     return 0;
 }
--- main.h
+++
@@ -1,9 +0,0 @@
-// MZMTIN002
-
-#ifndef ASSIGNMENT_4_MAIN_H
-#define ASSIGNMENT_4_MAIN_H
-
-#endif //ASSIGNMENT_4_MAIN_H
-
-using namespace std;
-#include "iostream"
